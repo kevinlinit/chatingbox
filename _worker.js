93541a2b01681775ddcdcd54d73a1f47b3df3805
kevinlinit <kevinlinit@gmail.com>
2024-07-04 22:12:56 +0800

    proxy

--- _worker.js
+++ _worker.js
@@ -172,8 +172,17 @@ export default {
 async function handleRequest(request) {
     const url = new URL(request.url);
 
-    // Check if the request path matches
-    if (url.pathname === "/login/device/code") {
+    if (url.pathname === "/") {
+        return new Response(page, {
+            headers: {
+                "content-type": "text/html;charset=UTF-8",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Methods": "*",
+            },
+        })
+    }else if (url.pathname === "/login/device/code") {
         // Fetch the original response
         const originalResponse = await fetch(request);
         const originalResponseData = await originalResponse.json();
@@ -188,13 +197,18 @@ async function handleRequest(request) {
             status: originalResponse.status
         });
     } else {
-        // For other paths, proxy the request without modification
+        const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
+        url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
         const modifiedRequest = new Request(url.toString(), {
             headers: request.headers,
             method: request.method,
             body: request.body,
             redirect: 'follow'
         });
-        return fetch(modifiedRequest);
+        const response = await fetch(modifiedRequest);
+        const modifiedResponse = new Response(response.body, response);
+        // 添加允许跨域访问的响应头
+        modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+        return modifiedResponse;
     }
 }
